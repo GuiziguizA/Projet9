548e5f44af741eb794f0fd0e1817d0cb26fc31d9
guillaume alisse <gualisse@gmail.com>
2020-09-07 16:16:56 +0200

    modification + test de la méthode isEquilibre

---
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/CompteComptableDaoCacheTest.java
@@ -0,0 +1,5 @@
+package com.dummy.myerp.consumer.dao.impl;
+
+public class CompteComptableDaoCacheTest {
+
+}
---
+++ src/myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptable.java
@@ -0,0 +1,73 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Bean représentant une séquence pour les références d'écriture comptable
+ */
+public class SequenceEcritureComptable {
+
+	// ==================== Attributs ====================
+	/** L'année */
+	@NotNull
+	private Integer annee;
+	/** La dernière valeur utilisée */
+	@NotNull
+	private Integer derniereValeur;
+
+	@NotNull
+	private JournalComptable journal;
+
+	// ==================== Constructeurs ====================
+	/**
+	 * Constructeur
+	 */
+	public SequenceEcritureComptable() {
+	}
+
+	/**
+	 * Constructeur
+	 *
+	 * @param pAnnee          -
+	 * @param pDerniereValeur -
+	 */
+	public SequenceEcritureComptable(Integer pAnnee, Integer pDerniereValeur) {
+		annee = pAnnee;
+		derniereValeur = pDerniereValeur;
+	}
+
+	// ==================== Getters/Setters ====================
+	public Integer getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(Integer pAnnee) {
+		annee = pAnnee;
+	}
+
+	public Integer getDerniereValeur() {
+		return derniereValeur;
+	}
+
+	public void setDerniereValeur(Integer pDerniereValeur) {
+		derniereValeur = pDerniereValeur;
+	}
+
+	public JournalComptable getJournal() {
+		return journal;
+	}
+
+	public void setJournal(JournalComptable journal) {
+		this.journal = journal;
+	}
+
+	// ==================== Méthodes ====================
+	@Override
+	public String toString() {
+		final StringBuilder vStB = new StringBuilder(this.getClass().getSimpleName());
+		final String vSEP = ", ";
+		vStB.append("{").append("annee=").append(annee).append(vSEP).append("derniereValeur=").append(derniereValeur)
+				.append("}");
+		return vStB.toString();
+	}
+}
