7fcb8843ef7bf5754a52e2598eca246a2645f9f9
guillaume alisse <gualisse@gmail.com>
2020-10-05 15:48:56 +0200

    jenkinsFile move

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Maven 3.3.6'
+	jdk '1.8.0_241'
+       
+    }
+    stages {
+        stage ('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
+
+        stage ('Build') {
+            steps {
+                sh 'mvn clean test' 
+            }
+            post {
+                success {
+                    junit 'target/surefire-reports/**/*.xml' 
+                }
+            }
+        }
+    }
+}
